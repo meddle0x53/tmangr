0db3369dfa5d18b3539a2641b49d4c575e4639f7
meddle <n.tzvetinov@gmail.com>
2013-10-30 09:32:42 +0200

    Fixed the tests with ember 1.1.2

--- test/javascripts/tasks_test.js
+++ test/javascripts/tasks_test.js
@@ -31,24 +31,26 @@ test("/tasks", function() {
 
 
 test("/tasks:sort", function() {
-  var task, store, expected = '<><>Should be first!<><>'
-  expect(1);
+  Ember.run(function() {
+    var task, store, expected = '<><>Should be first!<><>'
+    expect(1);
 
-  task = TaskManagement.Task.find(1);
-  store = task.get('store')
-  store.createRecord(TaskManagement.Task, {
-    description: expected,
-    state: 'opened',
-    created_at: '2986-04-27 12:10:11',
-    owner: TaskManagement.User.find(1),
-    performer: TaskManagement.User.find(3)
-  });
+    task = TaskManagement.Task.find(1);
+    store = task.get('store')
+    store.createRecord(TaskManagement.Task, {
+      description: expected,
+      state: 'opened',
+      created_at: '2986-04-27 12:10:11',
+      owner: TaskManagement.User.find(1),
+      performer: TaskManagement.User.find(3)
+    });
 
-  visit("/tasks").then(function() {
-    return click("a:contains('Description')")
-  }).then(function() {
-    first_desc = $.trim(find('tbody tr:first td.task-description').text())
-    equal(expected, first_desc)
+    visit("/tasks").then(function() {
+      return click("a:contains('Description')")
+    }).then(function() {
+      first_desc = $.trim(find('tbody tr:first td.task-description').text())
+      equal(expected, first_desc)
+    });
   });
 });
 
